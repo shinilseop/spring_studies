f8f791072b95a7fd2e262446c4b0ac72e5881631
shinilseop <shinilseop12@gmail.com>
2023-02-19 23:04:01 +0900

    add Configuration annotaion

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -15,7 +15,14 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
 
     @Bean
+    public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
@@ -25,12 +32,8 @@ public class AppConfig {
     }
 
     @Bean
-    public MemberService memberService() {
-        return new MemberServiceImpl(memberRepository());
-    }
-
-    @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 }
--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -18,4 +18,9 @@ public class OrderServiceImpl implements OrderService {
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
+
+    // test
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }

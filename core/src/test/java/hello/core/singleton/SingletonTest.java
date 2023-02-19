043e30d96365e4910e6e14a5b9fc76050a71d8f6
shinilseop <shinilseop12@gmail.com>
2023-02-19 21:45:52 +0900

    add singleton pattern test

---
+++ core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,30 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    public void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService1 = appConfig.memberService();
+        MemberService memberService2 = appConfig.memberService();
+
+        // memberServic1 != memberService2
+        Assertions.assertThat(memberService1).isNotSameAs(memberService2); // 다름
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    public void singletonServiceTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        Assertions.assertThat(singletonService1).isEqualTo(singletonService2);
+    }
+}

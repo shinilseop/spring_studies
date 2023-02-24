81ce812244ed29512d46d1f89b1c2f53458b5c12
shinilseop <shinilseop12@gmail.com>
2023-02-25 02:23:29 +0900

    refactoring springProvider -> JSR-330 Provider

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest.java
@@ -4,11 +4,13 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 public class SingletonWithPrototypeTest {
 
@@ -40,16 +42,18 @@ public class SingletonWithPrototypeTest {
     @Scope("singleton")
     static class ClientBean {
 
-        private final ObjectProvider<PrototypeBean> prototypeBeanObjectProvider;
-        private final ObjectFactory<PrototypeBean> prototypeBeanObjectFactory;
+        @Autowired
+        private Provider<PrototypeBean> prototypeBeanObjectProvider;
+//        private final ObjectProvider<PrototypeBean> prototypeBeanObjectProvider;
+//        private final ObjectFactory<PrototypeBean> prototypeBeanObjectFactory;
 
-        public ClientBean(ObjectProvider<PrototypeBean> prototypeBeanObjectProvider, ObjectFactory<PrototypeBean> prototypeBeanObjectFactory) {
-            this.prototypeBeanObjectProvider = prototypeBeanObjectProvider;
-            this.prototypeBeanObjectFactory = prototypeBeanObjectFactory;
-        }
+//        public ClientBean(ObjectProvider<PrototypeBean> prototypeBeanObjectProvider, ObjectFactory<PrototypeBean> prototypeBeanObjectFactory) {
+//            this.prototypeBeanObjectProvider = prototypeBeanObjectProvider;
+//            this.prototypeBeanObjectFactory = prototypeBeanObjectFactory;
+//        }
 
         public int logic() {
-            PrototypeBean prototypeBean = prototypeBeanObjectProvider.getObject();
+            PrototypeBean prototypeBean = prototypeBeanObjectProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }

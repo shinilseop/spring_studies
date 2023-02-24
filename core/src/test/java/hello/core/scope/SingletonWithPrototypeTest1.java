d8652d668e6ff9577240ca792c6f1820b1f451c7
shinilseop <shinilseop12@gmail.com>
2023-02-24 23:59:59 +0900

    prototype bean is independent test

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,38 +2,46 @@ package hello.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
-public class PrototypeTest {
+public class SingletonWithPrototypeTest1 {
+
     @Test
-    void singletonBeanFind() {
-        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+    void prototypeFind() {
+        var ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
 
         PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
-        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
-        System.out.println("prototypeBean1 = " + prototypeBean1);
-        System.out.println("prototypeBean2 = " + prototypeBean2);
-        Assertions.assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+        prototypeBean1.addCount();
+        Assertions.assertThat(prototypeBean1.getCount()).isEqualTo(1);
 
-        prototypeBean1.destroy();
-        prototypeBean2.destroy();
-        ac.close();
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        prototypeBean2.addCount();
+        Assertions.assertThat(prototypeBean2.getCount()).isEqualTo(1);
     }
 
     @Scope("prototype")
     static class PrototypeBean {
+        private int count = 0;
+
+        public void addCount() {
+            count++;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
         @PostConstruct
-        public void init(){
-            System.out.println("PrototypeBean.init");
+        public void init() {
+            System.out.println("PrototypeBean.init " + this);
         }
 
         @PreDestroy
-        public void destroy(){
+        public void destroy() {
             System.out.println("PrototypeBean.destroy");
         }
     }
